88148066145253e565331a67c2f8e27804017c88
rjpacket <jimbo922@163.com>
2018-04-19 18:17:40 +0800

    update 1

---
+++ src/com/company/_386/_5.java
@@ -0,0 +1,74 @@
+package com.company._386;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
+
+ 示例 1：
+
+ 输入: "babad"
+ 输出: "bab"
+ 注意: "aba"也是一个有效答案。
+ 示例 2：
+
+ 输入: "cbbd"
+ 输出: "bb"
+
+ * Created by An on 2018/4/19.
+ */
+public class _5 {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("aa"));
+    }
+
+    public static String longestPalindrome(String s) {
+        List<Character> a = new ArrayList<>();
+        List<Character> b = new ArrayList<>();
+        int count = 0;
+        String result = "";
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            if(count < length - i) {
+                a.clear();
+                b.clear();
+                int innerCount = 0;
+                for (int j = length - 1, k = i; k <= j && j >= 0; j--) {
+                    if (s.charAt(k) == s.charAt(j)) {
+                        if (k != j) {
+                            a.add(s.charAt(k));
+                            b.add(0, s.charAt(j));
+                        } else {
+                            a.add(s.charAt(k));
+                        }
+                        k++;
+                    } else {
+                        innerCount++;
+                        a.clear();
+                        b.clear();
+                        k = i;
+                        j = length - innerCount;
+                        if (innerCount >= length) {
+                            break;
+                        }
+                    }
+                }
+                int i1 = a.size() + b.size();
+                if (i1 > count) {
+                    count = i1;
+                    result = "";
+                    for (Character character : a) {
+                        result += String.valueOf(character);
+                    }
+
+                    for (Character character : b) {
+                        result += String.valueOf(character);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/com/company/_386/_6.java
@@ -0,0 +1,73 @@
+package com.company._386;
+
+/**
+ * 将字符串 "PAYPALISHIRING" 以Z字形排列成给定的行数：
+ * <p>
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后从左往右，逐行读取字符："PAHNAPLSIIGYIR"
+ * <p>
+ * 实现一个将字符串进行指定行数变换的函数:
+ * <p>
+ * string convert(string s, int numRows);
+ * 示例 1:
+ * <p>
+ * 输入: s = "PAYPALISHIRING", numRows = 3
+ * 输出: "PAHNAPLSIIGYIR"
+ * 示例 2:
+ * <p>
+ * 输入: s = "PAYPALISHIRING", numRows = 4
+ * 输出: "PINALSIGYAHRPI"
+ * 解释:
+ * <p>
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * Created by An on 2018/4/19.
+ */
+public class _6 {
+    public static void main(String[] args) {
+        System.out.println(convert("AB", 1));
+    }
+
+    public static String convert(String s, int numRows) {
+        if(numRows == 1){
+            return s;
+        }
+        int length = s.length();
+        String a[][] = new String[numRows][length];
+        M:for (int i = 0, j = 0, k = 0; i < length; ) {
+            for (int l = 0; l < numRows; l++) {
+                if (i >= length) {
+                    break M;
+                }
+                a[j][k] = String.valueOf(s.charAt(i));
+                j++;
+                i++;
+            }
+            j-=2;
+            k++;
+            for (int l = 0; l < numRows - 2; l++) {
+                if (i >= length) {
+                    break M;
+                }
+                a[j][k] = String.valueOf(s.charAt(i));
+                j--;
+                k++;
+                i++;
+            }
+
+        }
+        String result = "";
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < length; j++) {
+                if (a[i][j] != null) {
+                    result += a[i][j];
+                }
+            }
+        }
+        return result;
+    }
+}
